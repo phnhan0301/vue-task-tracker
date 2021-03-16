e4e1232a87f1c534b12339768c619f810ca7ddd0
Sunny Phan <nhan.phan@legato.co>
2021-03-17 00:09:52 +0700

    ADD styling design patten structure

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+@import "@/assets/styles/main.scss";
+</style>
+
 <template>
   <AppLayout>
     <router-view />
